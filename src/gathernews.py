f79dfb35dbedb3dc56208eb2d2f1034e148d3624
asmith564 <aaronsmith0709@gmail.com>
2020-06-10 08:35:11 -0500

    Finalized changes, added optional feature

--- src/gathernews.py
+++ src/gathernews.py
@@ -22,6 +22,8 @@ def SearchCNNArticle(input):
 		
 		if input in Keywords:
 			#Get the authors,title and summary
+			URL = CNN_article.url
+			MarkedURLs.append(URL)
 			Authors = CNN_article.authors
 			Title = CNN_article.title
 			Summary = CNN_article.summary
@@ -41,6 +43,8 @@ def SearchNYTArticle(input):
 		
 		if input in Keywords:
 			#Get the authors,title and summary
+			URL = NYT_article.url
+			MarkedURLs.append(URL)
 			Authors = NYT_article.authors
 			Title = NYT_article.title
 			Summary = NYT_article.summary
@@ -60,29 +64,29 @@ def SearchWPArticle(input):
 		
 		if input in Keywords:
 			#Get the authors,title and summary
+			URL = WP_article.url
+			MarkedURLs.append(URL)
 			Authors = WP_article.authors
 			Title = WP_article.title
 			Summary = WP_article.summary
 			DocumentArticle(Title, Authors, Summary)
 
 def DocumentArticle(Title, Author, Summary):
-	Document = open('news_summary.txt', 'w')
 	Document.write(Title + " - " + str(Author))
 	Document.write(Summary)
-	Document.close()
+	Document.write('			')
 
 
+
+Document = open('news_summary.txt', 'w')
 
 #Get the article
-
-CNN_URL = []
-NYT_URL = []
-WP_URL = []
-
 cnn_paper = newspaper.build('https://www.cnn.com/', language= 'en', memoize_articles=False, keep_article_html=True)
 NYTimes_paper = newspaper.build('https://www.nytimes.com/', language='en', memoize_articles=False, keep_article_html=True)
 WP_paper = newspaper.build('https://www.washingtonpost.com/', language='en', memoize_articles=False, keep_article_html=True)
 nltk.download('punkt')	    #1 time download of the sentence tokenizer
+
+MarkedURLs = []
 
 UserInput = Input()
 print("Please wait this will take a while . . . .")
@@ -90,6 +94,10 @@ print("Please wait this will take a while . . . .")
 SearchCNNArticle(UserInput)
 SearchNYTArticle(UserInput)
 SearchWPArticle(UserInput)
+
+Document.close()
+print(MarkedURLs)
+
 
 print("Fuction Finished, exiting")
 
